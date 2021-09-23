8d07f3241ab6e482196cb21a29eea63d417fd45d
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-09-23 08:20:58 +0000

    [devise]

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
-  #devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # devise_for :admins, controllers: {
-  #   sessions: "admins/sessions"
-  # }
-  
-  # namespace :admins do
-  #   resources :users, only: %i(index destroy)
-  #   resources :questions, only: %i(index destroy)
-  # end
 
-  # devise_for :users, controllers: {
-  #   sessions:      "users/sessions",
-  #   registrations: "users/registrations",
-  #   omniauth_callbacks: "users/omniauth_callbacks"
-  # }
+  devise_for :admins, controllers: {
+    sessions: "admins/sessions" ,
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations',
+  }
+  
+
+  devise_for :users, controllers: {
+    sessions:      "users/sessions",
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+  
+  root to: "home#index"
   
 end
